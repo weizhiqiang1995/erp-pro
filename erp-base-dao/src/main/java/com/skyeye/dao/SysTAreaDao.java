d783e7f11db821b7ea15c1b40f8d27e653694a24
weizhiqiang <598748873@qq.com>
2019-09-14 17:50:54 +0800

    新增行政区划

---
+++ erp-base-dao/src/main/java/com/skyeye/dao/SysTAreaDao.java
@@ -0,0 +1,18 @@
+package com.skyeye.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SysTAreaDao {
+
+	public List<Map<String, Object>> querySysTAreaList(Map<String, Object> map) throws Exception;
+
+	public List<Map<String, Object>> querySysTAreaProvinceList(Map<String, Object> map) throws Exception;
+
+	public List<Map<String, Object>> querySysTAreaCityList(Map<String, Object> map) throws Exception;
+
+	public List<Map<String, Object>> querySysTAreaChildAreaList(Map<String, Object> map) throws Exception;
+
+	public List<Map<String, Object>> querySysTAreaTownShipList(Map<String, Object> map) throws Exception;
+
+}
