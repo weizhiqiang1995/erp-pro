534af2e711cb6e5317bc5e3dea6badbcfdc9e454
weizhiqiang <598748873@qq.com>
2019-10-19 09:32:09 +0800

    其他出库功能完成

---
+++ erp-modular/src/main/java/com/skyeye/service/OtherOutLetsService.java
@@ -0,0 +1,16 @@
+package com.skyeye.service;
+
+import com.skyeye.common.object.InputObject;
+import com.skyeye.common.object.OutputObject;
+
+public interface OtherOutLetsService {
+
+	public void queryOtherOutLetsToList(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+	public void insertOtherOutLetsMation(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+	public void queryOtherOutLetsToEditById(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+	public void editOtherOutLetsMationById(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+}
