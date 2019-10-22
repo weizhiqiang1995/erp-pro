5468b2b49fefaa2baac3c5377f79a06fea8089f2
weizhiqiang <598748873@qq.com>
2019-10-22 10:17:03 +0800

    收预付款功能完成

---
+++ erp-modular/src/main/java/com/skyeye/service/AdvanceChargeService.java
@@ -0,0 +1,25 @@
+package com.skyeye.service;
+
+import com.skyeye.common.object.InputObject;
+import com.skyeye.common.object.OutputObject;
+
+/**
+ * @Author: 卫志强
+ * @Description: TODO
+ * @Date: 2019/10/20 10:22
+ */
+public interface AdvanceChargeService {
+	
+    public void queryAdvanceChargeByList(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+    public void insertAdvanceCharge(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+    public void queryAdvanceChargeToEditById(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+    public void editAdvanceChargeById(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+    public void deleteAdvanceChargeById(InputObject inputObject, OutputObject outputObject) throws Exception;
+
+    public void queryAdvanceChargeByDetail(InputObject inputObject, OutputObject outputObject) throws Exception;
+    
+}
---
+++ erp-modular/src/main/java/com/skyeye/service/impl/AdvanceChargeServiceImpl.java
@@ -0,0 +1,240 @@
+package com.skyeye.service.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
+import com.skyeye.common.object.InputObject;
+import com.skyeye.common.object.OutputObject;
+import com.skyeye.common.util.ToolUtil;
+import com.skyeye.dao.AdvanceChargeDao;
+import com.skyeye.erp.util.ErpConstants;
+import com.skyeye.erp.util.ErpOrderNum;
+import com.skyeye.service.AdvanceChargeService;
+
+import net.sf.json.JSONArray;
+
+/**
+ * @Author 卫志强
+ * @Description TODO
+ * @Date 2019/10/20 10:23
+ */
+@Service
+public class AdvanceChargeServiceImpl implements AdvanceChargeService {
+
+    @Autowired
+    private AdvanceChargeDao advanceChargeDao;
+
+    /**
+     * 查询收预付款列表信息
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @Override
+    public void queryAdvanceChargeByList(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        params.put("userId", inputObject.getLogParams().get("id"));
+        List<Map<String, Object>> beans = advanceChargeDao.queryAdvanceChargeByList(params,
+                new PageBounds(Integer.parseInt(params.get("page").toString()), Integer.parseInt(params.get("limit").toString())));
+        PageList<Map<String, Object>> beansPageList = (PageList<Map<String, Object>>) beans;
+        int total = beansPageList.getPaginator().getTotalCount();
+        outputObject.setBeans(beans);
+        outputObject.settotal(total);
+    }
+
+    /**
+     * 添加收预付款
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @SuppressWarnings("unchecked")
+	@Override
+    @Transactional(value="transactionManager")
+    public void insertAdvanceCharge(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        String initemStr = params.get("initemStr").toString();
+        if(ToolUtil.isJson(initemStr)) {
+            //财务主表ID
+            String useId = ToolUtil.getSurFaceId();
+            String userId = inputObject.getLogParams().get("id").toString();
+            //处理数据
+            JSONArray jArray = JSONArray.fromObject(initemStr);
+            //收预付款中间转换对象，财务子表存储对象
+            Map<String, Object> bean;
+            List<Map<String, Object>> entitys = new ArrayList<>();//财务子表实体集合信息
+            BigDecimal allPrice = new BigDecimal("0");//主单总价
+            BigDecimal itemAllPrice = null;//子单对象
+            for(int i = 0; i < jArray.size(); i++){
+                bean = jArray.getJSONObject(i);
+                Map<String, Object> entity = new HashMap<>();
+                //获取子项金额
+                itemAllPrice = new BigDecimal(bean.get("initemMoney").toString());
+                entity.put("id", ToolUtil.getSurFaceId());
+                entity.put("headerId", useId);
+                entity.put("accountId", bean.get("accountId"));
+                entity.put("eachAmount", bean.get("initemMoney"));
+                entity.put("remark", bean.get("remark"));
+                entity.put("userId", userId);
+                entity.put("deleteFlag", 0);
+                entitys.add(entity);
+                //计算总金额
+                allPrice = allPrice.add(itemAllPrice);
+            }
+            if(entitys.size() == 0){
+                outputObject.setreturnMessage("请选择账户");
+                return;
+            }
+            Map<String, Object> accountHead = new HashMap<>();
+            ErpOrderNum erpOrderNum = new ErpOrderNum();
+            String orderNum = erpOrderNum.getAccountOrderNumBySubType(userId, ErpConstants.AccountTheadSubType.ADVANCE_ORDER.getNum());
+            accountHead.put("id", useId);
+            accountHead.put("type", ErpConstants.AccountTheadSubType.ADVANCE_ORDER.getNum());//收预付款
+            accountHead.put("billNo", orderNum);
+            accountHead.put("totalPrice", allPrice);
+            accountHead.put("userId", userId);
+            accountHead.put("organId", params.get("organId"));
+            accountHead.put("operTime", params.get("operTime"));
+            accountHead.put("handsPersonId", params.get("handsPersonId"));
+            accountHead.put("remark", params.get("remark"));
+            accountHead.put("changeAmount", params.get("changeAmount"));
+            accountHead.put("deleteFlag", 0);
+            advanceChargeDao.insertAdvanceCharge(accountHead);
+            advanceChargeDao.insertAdvanceChargeItem(entitys);
+        }else{
+            outputObject.setreturnMessage("数据格式错误");
+        }
+    }
+
+    /**
+     * 查询收预付款用于数据回显
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @Override
+    public void queryAdvanceChargeToEditById(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        params.put("userId", inputObject.getLogParams().get("id"));
+        Map<String, Object> bean = advanceChargeDao.queryAdvanceChargeToEditById(params);
+        if(bean != null && !bean.isEmpty()){
+        	List<Map<String, Object>> beans = advanceChargeDao.queryAdvanceChargeItemsToEditById(params);
+        	bean.put("items", beans);
+        	//获取经手人员
+        	List<Map<String, Object>> userInfo = advanceChargeDao.queryUserInfoById(bean);
+        	bean.put("userInfo", userInfo);
+        	outputObject.setBean(bean);
+        	outputObject.settotal(1);
+        }else{
+        	outputObject.setreturnMessage("未查询到信息！");
+        }
+    }
+
+    /**
+     * 编辑收预付款信息
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @SuppressWarnings("unchecked")
+	@Override
+    @Transactional(value="transactionManager")
+    public void editAdvanceChargeById(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        String initemStr = params.get("initemStr").toString();
+        if(ToolUtil.isJson(initemStr)) {
+        	String useId = params.get("id").toString();
+        	String userId = inputObject.getLogParams().get("id").toString();
+            //处理数据
+            JSONArray jArray = JSONArray.fromObject(initemStr);
+            //收预付款中间转换对象，财务子表存储对象
+            Map<String, Object> bean;
+            List<Map<String, Object>> entitys = new ArrayList<>();//财务子表实体集合信息
+            BigDecimal allPrice = new BigDecimal("0");//主单总价
+            BigDecimal itemAllPrice = null;//子单对象
+            for(int i = 0; i < jArray.size(); i++){
+                bean = jArray.getJSONObject(i);
+                Map<String, Object> entity = new HashMap<>();
+                //获取子项金额
+                itemAllPrice = new BigDecimal(bean.get("initemMoney").toString());
+                entity.put("id", ToolUtil.getSurFaceId());
+                entity.put("headerId", useId);
+                entity.put("accountId", bean.get("accountId"));
+                entity.put("eachAmount", bean.get("initemMoney"));
+                entity.put("remark", bean.get("remark"));
+                entity.put("userId", userId);
+                entity.put("deleteFlag", "0");
+                entitys.add(entity);
+                //计算总金额
+                allPrice = allPrice.add(itemAllPrice);
+            }
+            if(entitys.size() == 0){
+                outputObject.setreturnMessage("请选择账户");
+                return;
+            }
+            Map<String, Object> accountHead = new HashMap<>();
+            accountHead.put("id", useId);
+            accountHead.put("userId", userId);
+            accountHead.put("totalPrice", allPrice);
+            accountHead.put("organId", params.get("organId"));
+            accountHead.put("operTime", params.get("operTime"));
+            accountHead.put("handsPersonId", params.get("handsPersonId"));
+            accountHead.put("remark", params.get("remark"));
+            accountHead.put("changeAmount", params.get("changeAmount"));
+            advanceChargeDao.editAdvanceChargeById(accountHead);
+            //删除之前的绑定信息
+            advanceChargeDao.deleteAdvanceChargeItemById(params);
+            advanceChargeDao.insertAdvanceChargeItem(entitys);
+        }else{
+            outputObject.setreturnMessage("数据格式错误");
+        }
+    }
+
+    /**
+     * 删除收预付款信息
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @Override
+    @Transactional(value="transactionManager")
+    public void deleteAdvanceChargeById(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        params.put("userId", inputObject.getLogParams().get("id"));
+        params.put("deleteFlag", 1);
+        advanceChargeDao.editAdvanceChargeByDeleteFlag(params);
+        advanceChargeDao.editAdvanceChargeItemsByDeleteFlag(params);
+    }
+
+    /**
+     * 查看收预付款详情
+     * @param inputObject
+     * @param outputObject
+     * @throws Exception
+     */
+    @Override
+    public void queryAdvanceChargeByDetail(InputObject inputObject, OutputObject outputObject) throws Exception {
+        Map<String, Object> params = inputObject.getParams();
+        params.put("userId", inputObject.getLogParams().get("id"));
+        //获取财务主表信息
+        Map<String, Object> bean = advanceChargeDao.queryAdvanceChargeDetailById(params);
+        if(bean != null && !bean.isEmpty()){
+            //获取子表信息
+            List<Map<String, Object>> beans = advanceChargeDao.queryAdvanceChargeItemsDetailById(bean);
+            bean.put("items", beans);
+            outputObject.setBean(bean);
+            outputObject.settotal(1);
+        }else{
+            outputObject.setreturnMessage("该数据已不存在.");
+        }
+    }
+}
